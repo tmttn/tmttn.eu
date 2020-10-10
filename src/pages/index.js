60f545fe8018b5fe670c1e5e564c82b49a4df62c
Thomas Metten <t.metten@aca-it.be>
2020-10-10 11:24:25 +0200

    ✍️ Add some content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
+import { Link } from "gatsby"
+
+import "../styles/main.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <section>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/blog/">Blog</Link>
+          <Link to="/about/">About</Link>
+        </nav>
+      </section>
+      <main id="welcome">
+        <h1>I'm Thomas Metten</h1>
+        <h3>
+          Developer | Father | Tabletop and Online RPG enthousiast
+          <span role="img" aria-label="roleplaying wizard">
+            🧙‍♂️
+          </span>
+        </h3>
+        <h2>Welcome to my site!</h2>
+      </main>
+    </div>
+  )
 }
