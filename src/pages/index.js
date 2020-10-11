ac34446c242c6a9306351c3fd5f50e8f98ad00a8
Thomas Metten <t.metten@aca-it.be>
2020-10-11 20:45:58 +0200

    ✏️ Change styling and add Layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,37 @@
 import React from "react"
-import Container from "../components/container"
+import Layout from "../components/layout"
 import Section from "../components/section"
 
 export default function Home() {
   return (
-    <Container>
+    <Layout>
       <header>
-        <img src="../../tmttn-logo-black-on-transparent.svg" alt="tmttn logo" class="logo"></img>
+        <img
+          src="../../tmttn-logo-black-on-transparent.svg"
+          alt="tmttn logo"
+          className="logo"
+        ></img>
         <nav>
-          <a href="welcome">Welcome</a>
-          <a href="about">About</a>
-          <a href="contact">Contact</a>
-          <a href="showcase">Showcase</a>
+          <a href="#welcome">Welcome</a>
+          <a href="#about">About</a>
+          <a href="#contact">Contact</a>
+          <a href="#showcase">Showcase</a>
         </nav>
       </header>
       <Section id="welcome">
         <main>
           <h1>Thomas Metten</h1>
-          <h3>
+          <h2>
             Developer | Father | Tabletop and Online RPG enthousiast
-            <span class="emoji" role="img" aria-label="roleplaying wizard">
+            <span className="emoji" role="img" aria-label="roleplaying wizard">
               🧙‍♂️
             </span>
-          </h3>
+          </h2>
         </main>
       </Section>
       <Section id="about">About</Section>
       <Section id="contact">Contact</Section>
       <Section id="showcase">Showcase</Section>
-    </Container>
+    </Layout>
   )
 }
