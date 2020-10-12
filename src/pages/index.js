1a75e884ab7031362a196bd80dcb7177403c6a88
Thomas Metten <t.metten@aca-it.be>
2020-10-12 18:13:43 +0200

    ✍️ Write bio, kinda...

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,6 +41,33 @@ export default function Home() {
         </Section>
         <Section id="about">
           <h1>About me</h1>
+          <div className="sectionContent">
+            <div>
+              <img
+                src="../../square-profile-picture.jpg"
+                alt="Thomas Metten"
+                heigth="260"
+                width="260"
+              ></img>
+            </div>
+            <div>
+              <p>
+                Hi! I'm Thomas (Tom) Metten, a {new Date().getFullYear() - 1987}{" "}
+                year old developer from Belgium. During the day, I work for{" "}
+                <a href="https://www.aca-it.be">ACA IT-Solutions</a>. After work
+                I spend quality time with my family, enjoy myself with some
+                personal and open-source coding projects and I bingewatching
+                Netflix / Disney+ / Youtube (a lot).
+              </p>
+              <p>
+                I also really enjoy tabletop boardgames, roleplaying games and
+                occasionally video games (I'm a PC gamer, but I do not shun the
+                consoles). Food is equally the bane of my existance as it is my
+                passion. I love cooking, but I enjoy eating even more. As you
+                can imagine the struggle against the scale is real.
+              </p>
+            </div>
+          </div>
         </Section>
         <Section id="contact">
           <h1>Contact</h1>
