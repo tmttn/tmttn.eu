1dd8d174fd3752c993da381087686ff526fc3eef
Thomas Metten <t.metten@aca-it.be>
2020-10-13 22:00:43 +0200

    ✍️ Fill contact section

---
+++ src/utils/fontawesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAt} from '@fortawesome/free-solid-svg-icons'
+import { faGithubSquare, faLinkedin, faTwitterSquare, faTwitch, faDiscord, faFacebookSquare,} from '@fortawesome/free-brands-svg-icons'
+library.add(faAt, faGithubSquare, faLinkedin, faTwitterSquare, faTwitch, faDiscord, faFacebookSquare)
