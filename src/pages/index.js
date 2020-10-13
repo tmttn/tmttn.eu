7a7e106b5b4c693249d91befe5ceb148f326da5a
Thomas Metten <t.metten@aca-it.be>
2020-10-13 22:00:43 +0200

    ✍️ Fill contact section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,8 @@ import Layout from "../components/layout"
 import Section from "../components/section"
 import Scrollable from "../components/scrollable"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "../utils/fontawesome"
 
 export default function Home() {
   return (
@@ -71,6 +73,40 @@ export default function Home() {
         </Section>
         <Section id="contact">
           <h1>Contact</h1>
+          <h2>Get in touch with me:</h2>
+          <div className="iconBar">
+            <a className="icons" href="https://github.com/tmttn">
+              <FontAwesomeIcon icon={["fab", "github-square"]} />
+            </a>
+
+            <a className="icons" href="https://twitter.com/tmetten">
+              <FontAwesomeIcon icon={["fab", "twitter-square"]} />
+            </a>
+
+            <a className="icons" href="https://www.linkedin.com/in/tmetten/">
+              <FontAwesomeIcon icon={["fab", "linkedin"]} />
+            </a>
+
+            <a className="icons" href="https://github.com/less-is-code">
+              <FontAwesomeIcon icon={["fab", "github-square"]} />
+            </a>
+
+            <a className="icons" href="mailto:contact@tmttn.be">
+              <FontAwesomeIcon icon={["fa", "at"]} />
+            </a>
+
+            <a className="icons" href="https://www.twitch.tv/bizarius">
+              <FontAwesomeIcon icon={["fab", "twitch"]} />
+            </a>
+
+            <a className="icons" href="https://discord.gg/G5E8XWR">
+              <FontAwesomeIcon icon={["fab", "discord"]} />
+            </a>
+
+            <a className="icons" href="https://www.facebook.com/mettentom">
+              <FontAwesomeIcon icon={["fab", "facebook-square"]} />
+            </a>
+          </div>
         </Section>
         <Section id="showcase">
           <h1>Showcase</h1>
